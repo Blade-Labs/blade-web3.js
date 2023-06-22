f249e851eb70c58ea3cf248c4d35bc557190990e
Ihor Zinchenko <igor.zinchenko@bladelabs.io>
2023-06-22 09:56:39 +0300

    WWT-2680: adjust the readme

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export { ErrorCodes, ErrorTypes, WalletError } from './src/models/wallet-errors'
 export { BladeWalletError } from './src/models/errors';
 export { BladeConnector } from './src/connector';
 export * from './src/models/blade';
+export * from './src/models/interfaces';
